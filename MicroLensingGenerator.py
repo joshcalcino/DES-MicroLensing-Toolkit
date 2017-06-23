d221b1489fe785b1de5d55126ce58478677965ed
Celeste Keith <ckeith2@wisc.edu>
2017-06-23 11:49:38 -0500

    June 23 morning

--- MicroLensingGenerator.py
+++ MicroLensingGenerator.py
@@ -8,29 +8,35 @@ import astropy.constants as const
 class GenerateMicrolensingEvent(object):
     """This class will generate a random microlensing event. All of the parameters are randomly generated."""
 
-    '''
+    """
     def get_pi_rel(self):
         pi_rel = np.random.uniform(low=0.01, high=0.1) # pi_rel (\pi_{\rm rel}) is the relative parallax between the
         # lens and the source, measured in milli-arcseconds.
         return pi_rel
-    '''
+    """
 
-    def __init__(self, t_0):
-        self.Ds, self.Dl, self.Drel = self.get_Dist()
-        self.Mass = self.get_Mass()
-        self.ImpactParameter = self.get_ImpactParameter()
-        self.t_E = self.get_t_E()
+    def __init__(self, t_0, p, V_t, M_lens, Ds, x, MJD_list, m_0, t_eff):
+        #self.Ds, self.Dl, self.Drel = self.get_Dist()
+        self.M_lens = M_lens
+        self.Ds = Ds
+        self.ImpactParameter = p
         self.t_0 = t_0
-        self.times = []
-
+        self.m_0 = m_0
+        self.t_eff = t_eff
+        self.times = MJD_list ##formerly []
+        self.x = x
+        self.V_t = V_t
+        self.t_E = self.get_t_E()
+        #self.generate_data()
+        #self.MJD_list = self.get_times(MJD_list)
+    """
     def get_Dist(self):
         Ds = np.random.uniform(low=1000, high=20000)  # Ds is the distance from the observer to the source.
-        Dl = np.random.uniform(low=0.1 * Ds, high=Ds)  # Dl is the distance from the observer to the lens.
-        #Ds = 1000
-        #Dl = 500
-        Drel = Ds - Dl  # Drel is the relative distance between the lens and the source.
+        Ds = 1000
+        Dl = 500
+        Drel = Ds - Dl  #Drel is the relative distance between the lens and the source.
         return [Ds, Dl, Drel]
-
+    """
     def get_Mass(self):
         M = np.random.uniform(low=0.1,
                               high=100) * const.M_sun.value  # the mass of the lens, relative to solar mass.
@@ -43,29 +49,36 @@ class GenerateMicrolensingEvent(object):
         return t_0
 
     def get_ImpactParameter(self):
+        """
         p = np.random.uniform(low=0.0,
-                              high=0.8)  # p is the impact paramater, lower p indicates a smaller lens to  # source
+                           high=0.8)  # p is the impact paramater, lower p indicates a smaller lens to  # source
+        """
+        p = self.ImpactParameter
         print 'p is'
         print p
         return p
 
     def get_r_E(self):  # r_E is the Einstein ring radius in units of.. not sure yet
-        M = self.Mass
-        Ds, Dl = self.Ds, self.Dl
+        M = self.M_lens
+        Ds = self.Ds
+        x = self.x
         # r_E = np.sqrt( (4*const.G * M)/const.c ** 2 * ( (Ds - Dl)/(Ds*Dl) ) )
-        r_E = 0.902 * np.sqrt(M / const.M_sun.value) * np.sqrt(10000 / Dl) * np.sqrt(
-            1 - Dl / Ds)  # in milli arcseconds
+        r_E = 0.902 * np.sqrt(M / const.M_sun.value) * np.sqrt(10000 / (x*Ds)) * np.sqrt(
+            1 - x)  # in milli arcseconds
         return r_E
 
-    def get_r_dot(self):  # relative proper motion between the lens and the source
+   """ May not need due to transverse velocity being an input parameter"""
+    '''
+    def get_r_dot(self):   relative proper motion between the lens and the source
         Ds, Dl = self.Ds, self.Dl
-        V = np.random.uniform(low=100, high=200) # relative transverse velocity of the lens with respect to the source
-        # V = 150
+        V = np.random.uniform(low=100, high=200)  relative transverse velocity of the lens with respect to the source
+        V = 150
         r_dot = 4.22 * (V / 200.0) * (10000 / Ds)
         return r_dot
+    '''
 
     def get_t_E(self):  # time it takes the source to move a distance equal to the Einstein ring radius
-        t_E = self.get_r_E() / self.get_r_dot()
+        t_E = self.get_r_E() / self.V_t #needs the same units as r_E to get out seconds
         print 't_E is'
         print t_E
         return t_E
@@ -77,28 +90,33 @@ class GenerateMicrolensingEvent(object):
         u = np.sqrt(p ** 2 + ((t - t_0) / t_E) ** 2)
         return u
 
+    def get_final_mag(self, t):
+        final_mag = self.get_delta_mag(t)+self.m_0
+        return final_mag
+
     def get_delta_mag(self, t):  # change in the magnitude of the star due to the lensing
         u = self.get_u(t)
         A = (u ** 2 + 2) / (u * np.sqrt(u ** 2 + 4))
         delta_mag = 2.5 * np.log10(A)
         return delta_mag
 
-    def get_times(self, MJD_list):
-        self.times = MJD_list
-
+    def get_times(self):
+        #self.times = MJD_list
+        return self.times
+    """
     def generate_times(self):
-        # t = np.random.uniform(low=0, high=5, size=50)  # 10 random points to mimic the DES fields
-        # t = np.sort(t)
-        # print t
+        t = np.random.uniform(low=0, high=5, size=50)  # 10 random points to mimic the DES fields
+        t = np.sort(t)
+        print t
         t = np.linspace(0, 5, 10)
         return t
-
+    """
     def generate_data(self):
-        t = self.generate_times()
-        delta_mag = self.get_delta_mag(t) - self.generate_noise(t)
-        errors = self.generate_errors(t)
-        data = {"delta_mag": delta_mag, "time": t, "errors": errors}
-        return data
+        t = self.get_times()
+        delta_mag_list = self.get_delta_mag(t)
+        mag_list = self.m_0 + delta_mag_list
+        final_mag_list = mag_list + self.generate_noise()
+        return final_mag_list
 
     def simple_fake(self):
         t = self.times()
@@ -122,5 +140,5 @@ class GenerateMicrolensingEvent(object):
         return errors
 
     def generate_noise(self, t):
-        noise = np.random.normal(0.0, 0.035, len(t))
+        noise = 0 
         return noise
