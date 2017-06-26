b46efa90ddc31ac3d41022296f9a44c08331fdc0
Celeste Keith <ckeith2@wisc.edu>
2017-06-26 16:35:00 -0500

    fake plots 2

--- fake_plots.py
+++ fake_plots.py
@@ -9,18 +9,21 @@ from scipy.optimize import newton
 from scipy.interpolate import interp1d
 
 
-x = MicroLensingGenerator.GenerateMicrolensingEvent(50, 0.1, 1e-15, 30, 4.5, 0.5, np.arange(1, 100, 0.01), 12, 1, 1)
+x = MicroLensingGenerator.GenerateMicrolensingEvent(1, 0.1, 220, 30, 4.5, 0.5, np.arange(1, 100, 0.01), 12, 1, 1)
 
 t = x.times
 u = x.get_u(t)
 A = x.A
+delta_mag = x.get_delta_mag(t)
 interp = interp1d(u, A, bounds_error =  False, kind = 'linear')
 
 plt.plot(u, 2.5*np.log10(interp(u)))
+#plt.plot(u, interp2(delta_mag))
 plt.ylim(0, 0.5)
 plt.xlabel("Einstein Radii")
 plt.ylabel("Magnitude Difference")
 plt.grid()
+plt.title("Fake graph with chosen inputs")
 plt.show()
 
 
