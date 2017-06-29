099818c20972bca370b46a408e3fdc2c82ac87f7
Marika McGhee <marika@fnal.gov>
2017-06-29 17:22:16 -0500

    thursday

--- fake_plots.py
+++ fake_plots.py
@@ -1,5 +1,4 @@
 import MicroLensingGenerator
-import getData
 import matplotlib.pyplot as plt
 import matplotlib
 from astropy.io import fits
@@ -12,13 +11,16 @@ import sys
 import pandas as pd
 import matplotlib as mpl
 
+""" 
+
+    """
+
+
 """  Argument inputs:
         t_0, p, V_t, M_lens, Ds, x, MJD_list, m_0, t_eff, curve_type"""
-def nike() :
-    #data = getData.getData(11737)
-    #MJD_list = data.get_MJD(6,'g')
+def nike(MJD_list) :
     x = MicroLensingGenerator.GenerateMicrolensingEvent(56948, 0.1, 220, 30, 4.5, 0.5, MJD_list, 12, 1, 1)
-    # x = MicroensingGenerator.GenerateMicrolensingEvent(1, 0.1, 220, 30, 4.5, 0.5, np.arange(1, 1000, 2), 12, 1, 1)
+    # x = MicroLensingGenerator.GenerateMicrolensingEvent(1, 0.1, 220, 30, 4.5, 0.5, np.arange(1, 1000, 2), 12, 1, 1)
     t = x.times
     u = x.get_u(t)
     A = x.A
@@ -29,12 +31,9 @@ def nike() :
     sys.path.append('/data/des51.b/data/neilsen/wide_cadence/python')
 
 
-    print len(u), "u: ", u
-    print len(A), "A: ", A
     interp = interp1d(u, A, bounds_error =  False, kind = 'linear')
     utime = np.arange(u.min(),u.max(),0.001)
     dm = 2.5*np.log10(interp(utime))
-  
 
     
     plt.clf()
